1c011ed84788995da905e39f6dec5a47d666ad02
SosukeKato <dokindokinpiano@gmail.com>
2025-10-10 17:02:25 +0900

    コメント追加

--- Assets/Scripts/Figures.cs
+++ Assets/Scripts/Figures.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Figures : MonoBehaviour
 {
+    //ƒRƒCƒ“‚ÌŠ—L”
     public int _wallet;
+    //Player‚ÌHP
     public int _playerHP;
-
+    
     public bool _death;
 
     void Update()
