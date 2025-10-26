2da2cfd44841b56ebb29de26112753870762c844
junyoko1 <junyoko1@outlook.jp>
2025-10-26 22:09:45 +0900

    [ add ]シールドのスクリプトとシールドのプレハブを追加

---
+++ Assets/Scripts/ShieldPanel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShieldPanel : Panel
+{
+    [SerializeField, Header("シールド量")]
+    private int _shield = 5;
+
+    public override void Effect(PreviewPlayerData preview)
+    {
+        preview.Shield +=  _shield;
+    }
+    public override void DestroyThis()
+    {
+        //ReferenceManager.Instance.ChouBoardManager.RemovePanelFromBoard(this);
+        Destroy(gameObject);
+    }
+}
