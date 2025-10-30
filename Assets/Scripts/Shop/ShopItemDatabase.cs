16a47d742cc301c34cbabc1f1964232f64b02aa6
ayato0913a <ayato0913a@gmail.com>
2025-10-31 01:20:28 +0900

    [ feat ] ”こっちが本当の”強化するアイテムのデータのリストデータを管理

---
+++ Assets/Scripts/Shop/ShopItemDatabase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "Shop/ItemDatabase")]
+/// <summary>
+///         アイテム一覧をまとめるSO
+/// </summary>
+public class ShopItemDatabase : ScriptableObject
+{
+    public List<ShopItemData> items;
+}
