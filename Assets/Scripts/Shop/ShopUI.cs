3ea6dea1b922660a15208c287cc9daddbf3f286c
ayato0913a <ayato0913a@gmail.com>
2025-10-31 01:12:03 +0900

    [ feat ] 一アイテム強化分のUIを制御するクラスを作成

---
+++ Assets/Scripts/Shop/ShopUI.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+///         1アイテム分のUI表示
+/// </summary>
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField] private Image _icon;
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private TMP_Text _descText;
+    [SerializeField] private Button _buyButton;
+
+    private ShopItemData _shopItemData;
+    private ShopUIManager _shopUIManager;
+
+    /// <summary>
+    ///         UIのテキストに値を設定
+    /// </summary>
+    /// <param name="shopItemData"></param>
+    /// <param name="manager"></param>
+    public void Setup(ShopItemData shopItemData, ShopUIManager manager)
+    {
+        _shopItemData = shopItemData;
+        _shopUIManager = manager;
+
+        _icon.sprite = _shopItemData.Icon;
+        _nameText.text = _shopItemData.ItemName;
+        _descText.text = $"{_shopItemData.Description} {_shopItemData.Value}";
+
+        _buyButton.onClick.AddListener(HandleBuy);
+    }
+
+    /// <summary>
+    ///         ボタンを使ったイベント
+    /// </summary>
+    private void HandleBuy()
+    {
+        _shopUIManager.BuyItem(_shopItemData);
+        _shopUIManager.CloseShop();
+    }
+}
