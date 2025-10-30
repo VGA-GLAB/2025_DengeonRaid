f46832e523ea23b8fb7a8d038fe53f55d967067b
ayato0913a <ayato0913a@gmail.com>
2025-10-31 01:14:34 +0900

    [ feat ] 強化するアイテムのデータ保存するScriptableobjectの定義

---
+++ Assets/Scripts/Shop/ShopItemData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Shop/Item")]
+public class ShopItemData : ScriptableObject
+{
+    public ShopEffectType ShopEffectType;
+    public string ItemName;
+    public Sprite Icon;
+    public int Value;
+    [TextArea,Range(0,2)]public string Description;
+}
+
+public enum ShopEffectType
+{
+    MaxHp,
+    MaxShild,
+    Attack,
+    Gold
+}
