bcafbb5a252a0d5531330b517ddba224167db4e0
ayato0913a <ayato0913a@gmail.com>
2025-11-02 20:16:05 +0900

    [ feat ] Shopの現在ステータスの情報を表示するためのスクリプトを作成

---
+++ Assets/Scripts/Shop/ShopPlayerStutsUI.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+
+/// <summary>
+///         PlayerのステータスUI表示
+/// </summary>
+public class ShopPlayerStutsUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _hpText;
+    [SerializeField] private TMP_Text _attackText;
+    [SerializeField] private TMP_Text _shieldText;
+
+    private PreviewPlayerData _previewPlayerData;
+
+    /// <summary>
+    ///         初期化
+    /// </summary>
+    /// <param name="playerData"></param>
+    public void SetUp(PreviewPlayerData playerData)
+    {
+        _previewPlayerData = playerData;
+        UpdateUI();
+    }
+
+    /// <summary>
+    ///         UIの更新
+    /// </summary>
+    private void UpdateUI()
+    {
+        _hpText.text = $"HP: {_previewPlayerData.Hp} / {_previewPlayerData.HpMax}";
+        _attackText.text = $"Attack: {_previewPlayerData.BaseAttack}";
+        _shieldText.text = $"Shield: {_previewPlayerData.Shield} / {_previewPlayerData.ShieldMax}";
+    }
+}
