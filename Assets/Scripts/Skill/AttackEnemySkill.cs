6d131aa3094eb67362b9dbbe8cc09cb87325414a
ayato0913a <ayato0913a@gmail.com>
2025-11-07 00:06:04 +0900

    [ feat ] 敵への攻撃スキルを作成

---
+++ Assets/Scripts/Skill/AttackEnemySkill.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+///         敵への全体攻撃スキル
+/// </summary>
+public class AttackEnemySkill : SkillBase
+{
+    [SerializeField] private int _skillDamageRatio = 3;
+
+    public override void ActivateSkill()
+    {
+        Debug.Log("敵への攻撃スキル発動");
+        List<EnemyPanel> enemiesPanel = _boardManager.GetEnemyPanels();
+
+        foreach (var enemy in enemiesPanel)
+        {
+            Debug.Log($"敵パネルID: {enemy} に攻撃を行いました。");
+            PreviewEnemyData enemyPreview = new PreviewEnemyData(enemy);
+
+            enemyPreview.Hp /= _skillDamageRatio;
+            enemy.SetResolvePreview(enemyPreview);
+            enemy.ApplyPreview();
+            enemy.UpdateAttrDisplay();
+        }
+    }
+}
