d00b48f1d302e7c3c2998160ece856fea8a43b23
ayato0913a <ayato0913a@gmail.com>
2025-11-07 03:44:21 +0900

    [ feat ] 盤面のハートを吸収して回復するスキルを追加

---
+++ Assets/Scripts/Skill/HeartCollectSkill.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HeartCollectSkill : SkillBase
+{
+    public override void ActivateSkill()
+    {
+        Panel[,] panels = _boardManager.GetBoardArray;
+        foreach (var panel in panels)
+        {
+            if (panel is PotionPanel potionPanel)
+            {
+                _boardManager.DeleatePanel(potionPanel.BoardPos);
+            }
+        }
+    }
+}
