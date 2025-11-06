e08b7754e39254f846fa1f667be94cd1bf6a8852
ayato0913a <ayato0913a@gmail.com>
2025-11-07 03:51:18 +0900

    [ fix ] コメント追加

--- Assets/Scripts/Skill/AttackEnemySkill.cs
+++ Assets/Scripts/Skill/AttackEnemySkill.cs
@@ -11,11 +11,13 @@ public class AttackEnemySkill : SkillBase
     public override void ActivateSkill()
     {
         Debug.Log("敵への攻撃スキル発動");
+        //  敵パネル全取得
         List<EnemyPanel> enemiesPanel = _boardManager.GetEnemyPanels();
 
         foreach (var enemy in enemiesPanel)
         {
             Debug.Log($"敵パネルID: {enemy} に攻撃を行いました。");
+            //  プレビュー計算
             PreviewEnemyData enemyPreview = new PreviewEnemyData(enemy);
 
             enemyPreview.Hp /= _skillDamageRatio;
