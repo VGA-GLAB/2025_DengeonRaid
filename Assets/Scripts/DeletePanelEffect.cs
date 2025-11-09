cf83a191336cfe79aafe9c36d28c39ab81329b48
SosukeKato <dokindokinpiano@gmail.com>
2025-11-09 16:10:48 +0900

    [update]消去されたパネルが移動しながらサイズを変更する処理を追加

--- Assets/Scripts/DeletePanelEffect.cs
+++ Assets/Scripts/DeletePanelEffect.cs
@@ -12,19 +12,27 @@ public class DeletePanelEffect : MonoBehaviour
     [SerializeField, Header("移動速度")]
     private float _moveSpeed;
 
-    [SerializeField, Header("パネルのサイズ")]
-    private Vector3 _panelSize;
+    [SerializeField, Header("縮小時のパネルのサイズ")]
+    private Vector3 _panelMinimalizeSize;
 
-    [SerializeField, Header("パネルのサイズ変更速度")]
-    private float _panelScaleSpeed;
+    [SerializeField, Header("縮小時のパネルのサイズ変更速度")]
+    private float _panelMinimalizeSpeed;
+
+    [SerializeField, Header("拡大時のパネルのサイズ")]
+    private float _panelExpantionSpeed;
+
+    [SerializeField, Header("拡大時のパンルのサイズ変更速度")]
+    private float _panelExpantionSize;
 
     public void PanelMove()
     {
-        transform.DOPath(new Vector3[] { _viaPos, _targetPos }, _moveSpeed, PathType.CatmullRom);
+        DOTween.Sequence()
+            .Join(transform.DOPath(new Vector3[] { _viaPos, _targetPos }, _moveSpeed, PathType.CatmullRom))
+            .Join(transform.DOScale(_panelExpantionSize, _panelExpantionSpeed));
     }
 
     public void PanelScale()
     {
-        transform.DOScale(_panelSize, _panelScaleSpeed);
+        transform.DOScale(_panelMinimalizeSize, _panelMinimalizeSpeed);
     }
 }
