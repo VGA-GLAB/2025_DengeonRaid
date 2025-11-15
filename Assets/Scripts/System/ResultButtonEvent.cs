3047534fcde5a2da959595640503fde0fc9c6387
ChouTakushin <takushin.chou@gmail.com>
2025-11-16 03:27:56 +0900

    [add] タイトル・リザルト画面の実装

---
+++ Assets/Scripts/System/ResultButtonEvent.cs
@@ -0,0 +1,20 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResultButtonEvent : MonoBehaviour
+{
+    [SerializeField] private Image _fadeInImage;
+
+    public void OnButtonClicked(string sceneName)
+    {
+        _fadeInImage.gameObject.SetActive(true);
+        _fadeInImage.DOFade(1f, 1f).OnComplete(LoadTitleScene);
+    }
+
+    private void LoadTitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
---
+++ Assets/Scripts/UI/WipeInController.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+public class WipeInController : MonoBehaviour
+{
+    [SerializeField] private GameObject _wipeInImage;
+    void Start()
+    {
+        _wipeInImage.transform.DOMoveY(2000f, 1f).SetEase(Ease.Linear).OnComplete(() => _wipeInImage.SetActive(false));
+    }
+}
