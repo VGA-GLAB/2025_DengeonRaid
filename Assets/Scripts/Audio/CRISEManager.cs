79ddf8a8fcab0a7d6b90135dd81eeb25a94e36c9
ayato0913a <ayato0913a@gmail.com>
2025-12-01 20:40:19 +0900

    [ feat ] 機能作成

--- Assets/Scripts/Audio/CRISEManager.cs
+++ Assets/Scripts/Audio/CRISEManager.cs
@@ -1,16 +1,48 @@
+﻿using CriWare;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class CRISEManager : MonoBehaviour
+public class CRISEManager
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private List<SEPlayer> _pool = new List<SEPlayer>();
+    private CriAtomExAcb _criAtomExAcb;
+    private int _poolSize = 10;
+
+    public void Initialize(CriAtomExAcb criAtomExAcb)
     {
-        
+        _criAtomExAcb = criAtomExAcb;
+
+        for (int i = 0; i < _poolSize; i++)
+        {
+            _pool.Add(new SEPlayer(_criAtomExAcb));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolome(float volume)
     {
-        
+        foreach (var p in _pool)
+        {
+            p?.SetVolume(volume);
+        }
+    }
+
+    /// <summary>
+    ///         
+    /// </summary>
+    /// <param name="cueName"></param>
+    public void PlaySEDesignation(string cueName)
+    {
+        SEPlayer free = _pool.Find(p => !p.IsPlaying);
+        if (free != null)
+            free.Play(cueName);
+    }
+
+    public void DisposeAll()
+    {
+        foreach (var p in _pool)
+        {
+            p?.Dispose();
+        }
+        _pool.Clear();
     }
 }
