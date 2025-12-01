ffb290bc237131beccff9ee8448b2f0ad1d9794d
ayato0913a <ayato0913a@gmail.com>
2025-12-01 20:39:54 +0900

    [ feat ] 機能作成

--- Assets/Scripts/Audio/CRIBGMManager.cs
+++ Assets/Scripts/Audio/CRIBGMManager.cs
@@ -1,16 +1,54 @@
+﻿using CriWare;
 using UnityEngine;
+using System;
 
-public class CRIBGMManager : MonoBehaviour
+/// <summary>
+///         単一BGMの管理クラス
+/// </summary>
+public class CRIBGMManager
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private CriAtomExPlayer _player;
+    private CriAtomExAcb _criAtomExAcb;
+
+    /// <summary>
+    ///         初期化
+    /// </summary>
+    /// <param name="criAtomExAcb"></param>
+    public void Init(CriAtomExAcb criAtomExAcb)
     {
-        
+        _criAtomExAcb = criAtomExAcb;
+        if (_player == null) _player = new CriAtomExPlayer();
+        SetVolume(SoundSettings.BGMVolume);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    ///         CriAtomExPlayerに音量を設定
+    /// </summary>
+    /// <param name="volume">SoundSettingsに設定している音量</param>
+    public void SetVolume(float volume)
     {
-        
+        if (_player != null) _player.SetVolume(Mathf.Clamp01(volume));
+    }
+
+    /// <summary>
+    ///         BGMの再生
+    /// </summary>
+    /// <param name="cueName">再生するBGMの名前</param>
+    public void Play(string cueName)
+    {
+        if (_criAtomExAcb == null) return;
+
+        CriAtomEx.CueInfo[] cueArray = _criAtomExAcb.GetCueInfoList();
+        CriAtomEx.CueInfo info = Array.Find(cueArray, ci => ci.name == cueName);
+        _player.SetCue(_criAtomExAcb, info.id);
+        _player.Start();
+    }
+
+    /// <summary>
+    ///         BGMの停止
+    /// </summary>
+    public void Stop()
+    {
+        _player?.Stop();
     }
 }
