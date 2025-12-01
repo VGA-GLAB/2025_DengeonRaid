004c6642d1ce9b165084a34cb960c199622c7353
ayato0913a <ayato0913a@gmail.com>
2025-12-01 20:38:13 +0900

    [ feat ] 音量を外部から変更されたものを保存、取得する用のクラスを作成

---
+++ Assets/Scripts/Audio/SoundSettings.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+///         音量を保存、取得するユーティリティクラス
+///         最大音量を1として01で制御
+/// </summary>
+public static class SoundSettings
+{
+    private const string KEY_BGM = "Audio_BGM_Volume";
+    private const string KEY_SE = "Aduio_SE_Volume";
+
+    public static float BGMVolume
+    {
+        get => PlayerPrefs.GetFloat(KEY_BGM, 1.0f);
+        set
+        {
+            PlayerPrefs.SetFloat(KEY_BGM, Mathf.Clamp01(value));
+            PlayerPrefs.Save();
+        }
+    }
+
+    public static float SEVolume
+    {
+        get => PlayerPrefs.GetFloat(KEY_SE, 1.0f);
+        set
+        {
+            PlayerPrefs.SetFloat(KEY_SE, Mathf.Clamp01(value));
+            PlayerPrefs.Save();
+        }
+    }
+}
