45b2ea846ce4ea54d6ffe50cb7458e6062ad1731
ayato0913a <ayato0913a@gmail.com>
2025-12-01 20:41:05 +0900

    [ feat ] Audio機能を統括するクラスを作成中

---
+++ Assets/Scripts/Audio/CRIAudioManager.cs
@@ -0,0 +1,25 @@
+﻿using CriWare;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRIAudioManager
+{
+    public static CRIAudioManager _instance { get; } = new CRIAudioManager();
+
+    public CRIBGMManager BGMManager { get; private set; } = new CRIBGMManager();
+    public CRISEManager SEManager { get; private set; } = new CRISEManager();
+
+    private bool _isReady = false;  
+    private Queue<Action> _deferredActions = new Queue<Action>();
+
+    public async void InitializeAsync(CriAtomExAcb bgmAcb, CriAtomExAcb seAcb)
+    {
+        CriAtom atom = GameObject.FindObjectOfType<CriAtom>();
+        if (atom == null)
+        {
+            Debug.LogError("CriAtomがシーン内に存在しない");
+            return;
+        }
+    }
+}
