cbd783e475848c2b411ed8288ef865e30cf876a5
ayato0913a <ayato0913a@gmail.com>
2025-12-04 17:45:03 +0900

    [ feat ] Test用のAudioPlayerを作成

---
+++ Assets/Scripts/Audio/TestAudioPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class TestAudioPlayer : MonoBehaviour
+{
+    private async void Start()
+    {
+        // AudioManagerの読み込み待ち
+        await UniTask.WaitUntil(() => CRIAudioManager.Instance.IsReady);
+
+        Debug.Log("AudioManager Ready!");
+
+        // BGM 再生
+        CRIAudioManager.Instance.BGMManager.Play("ME_Gameover");
+
+        // 1秒後に SE 再生
+        await UniTask.Delay(1000);
+        CRIAudioManager.Instance.SEManager.Play("SE_Click");
+    }
+}
