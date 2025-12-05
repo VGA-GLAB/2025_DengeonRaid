eef171e05ce7bc09dc8b75982a5372f3ce723bfc
ayato0913a <ayato0913a@gmail.com>
2025-12-06 04:20:41 +0900

    [ fix ] Playの仕方を調整

--- Assets/Scripts/Audio/TestAudioPlayer.cs
+++ Assets/Scripts/Audio/TestAudioPlayer.cs
@@ -6,15 +6,15 @@ public class TestAudioPlayer : MonoBehaviour
     private async void Start()
     {
         // AudioManagerの読み込み待ち
-        await UniTask.WaitUntil(() => CRIAudioManager.Instance.IsReady);
+        await CRIAudioManager.ReadyTask();
 
         Debug.Log("AudioManager Ready!");
 
         // BGM 再生
-        CRIAudioManager.Instance.BGMManager.Play("ME_Gameover");
+        CRIAudioManager.CRIBGMManager.Play("ME_Gameover");
 
         // 1秒後に SE 再生
         await UniTask.Delay(1000);
-        CRIAudioManager.Instance.SEManager.Play("SE_Click");
+        CRIAudioManager.CRISEManager.Play("SE_Click");
     }
 }
