913ca372088e00374117c49fea8994743b71a143
ayato0913a <ayato0913a@gmail.com>
2025-12-07 07:37:46 +0900

    [ feat ] Sceneが読み込まれたときにBGMを鳴らすクラスを作成

---
+++ Assets/Scripts/Audio/SceneBGMPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SceneBGMPlayer : MonoBehaviour
+{
+    [SerializeField, Header("シーン開始時に再生するBGMの名前")]
+    private string _bgmName;
+
+    private async void Start()
+    {
+        // AudioManagerの読み込み待ち
+        await CRIAudioManager.ReadyTask();
+
+        // BGM 再生
+        CRIAudioManager.CRIBGMManager.Play(_bgmName);
+    }
+}
