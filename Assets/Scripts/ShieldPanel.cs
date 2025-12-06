831ed173aa358f839245b8fb3db130b3aff56ff2
ayato0913a <ayato0913a@gmail.com>
2025-12-06 23:28:40 +0900

    [ fix ] Eddect呼び出しの処理を追加

--- Assets/Scripts/ShieldPanel.cs
+++ Assets/Scripts/ShieldPanel.cs
@@ -7,10 +7,12 @@ public class ShieldPanel : Panel
 
     public override void Effect(PreviewPlayerData preview)
     {
-        preview.Shield +=  _shield;
+        preview.Shield += _shield;
     }
     public override void DestroyThis()
     {
+        DeletePanelEffectManager.Instance.EffectMove(this);
+
         ReferenceManager.Instance.BoardManager.RemovePanelFromBoard(this);
         Destroy(gameObject);
     }
