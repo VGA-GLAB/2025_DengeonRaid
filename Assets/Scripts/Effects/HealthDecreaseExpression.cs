183bfa211be61ad6f5d0fdfeeb09d332046d95f9
SosukeKato <dokindokinpiano@gmail.com>
2025-12-06 22:39:48 +0900

    [update]HPが少しずつ減っていく処理を追加

--- Assets/Scripts/Effects/HealthDecreaseExpression.cs
+++ Assets/Scripts/Effects/HealthDecreaseExpression.cs
@@ -1,6 +1,35 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthDecreaseExpression : MonoBehaviour
 {
+    [SerializeField]
+    private PreviewPlayerData _ppd;
+    [SerializeField]
+    private Slider _hpGauge;
 
+    private int _beforeHp;
+
+    /// <summary>
+    /// 減少前のHPを取得(パネル消す前に実行してもらうつもりで書いたやつ)
+    /// </summary>
+    public void GetBeforeHP()
+    {
+        _beforeHp = _ppd.Hp;
+        _hpGauge.value = _beforeHp;
+    }
+
+    /// <summary>
+    /// 減少した分のHPを減らす処理
+    /// </summary>
+    public IEnumerator DecreaseGauge()
+    {
+        while (_beforeHp > _ppd.Hp)
+        {
+            _beforeHp--;
+            _hpGauge.value = _beforeHp;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
 }
