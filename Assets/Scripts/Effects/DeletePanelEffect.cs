7fd3e6106759feabb1d8a45ccd6a415351aea604
ayato0913a <ayato0913a@gmail.com>
2025-12-06 23:29:26 +0900

    [ fix ] 調整

--- Assets/Scripts/Effects/DeletePanelEffect.cs
+++ Assets/Scripts/Effects/DeletePanelEffect.cs
@@ -9,15 +9,6 @@ public class DeletePanelEffect : MonoBehaviour
     [SerializeField, Header("移動速度")]
     private float _moveSpeed;
 
-    [SerializeField, Header("Panel選択時に縮小するパネルのサイズ")]
-    private float _panelMinimalizeSize;
-
-    [SerializeField, Header("Panel選択時に縮小するパネルのサイズ変更速度")]
-    private float _panelMinimalizeSpeed;
-
-    [SerializeField,Header("Panelの選択を解除したときにサイズを戻す速度")]
-    private float _panelReturnSpeed;
-
     [SerializeField, Header("移動時に縮小する時のパネルのサイズ")]
     private float _panelMoveMinimalizeSize;
 
@@ -33,28 +24,12 @@ public class DeletePanelEffect : MonoBehaviour
     /// <summary>
     ///         消去したパネルをアニメーションで移動させる
     /// </summary>
-    public void PanelMove(Transform viaObj,Transform targetObj,float delay)
+    public void PanelMove(Transform viaObj, Transform targetObj)
     {
-        transform.DOPath(new Vector3[] {viaObj.position,targetObj.position }, _moveSpeed, PathType.CatmullRom);
+        transform.DOPath(new Vector3[] { viaObj.position, targetObj.position }, _moveSpeed, PathType.CatmullRom);
         DOTween.Sequence()
-            .AppendInterval(delay)
             .Append(transform.DOScale(_panelExpantionSize, _panelExpantionSpeed))
-            .Append(transform.DOScale(_panelMoveMinimalizeSize, _panelMoveMinimalizeSpeed));
-    }
-
-    /// <summary>
-    ///         選択されたときにパネルを縮小させる
-    /// </summary>
-    public void PanelScale()
-    {
-        transform.DOScale(_panelMinimalizeSize, _panelMinimalizeSpeed);
-    }
-
-    /// <summary>
-    ///         選択が解除されたときにパネルを元のサイズに戻す
-    /// </summary>
-    public void ReturnPanelScale()
-    {
-        transform.DOScale(1f, _panelReturnSpeed);
+            .Append(transform.DOScale(_panelMoveMinimalizeSize, _panelMoveMinimalizeSpeed)
+            .OnComplete(() => Destroy(gameObject)));
     }
 }
