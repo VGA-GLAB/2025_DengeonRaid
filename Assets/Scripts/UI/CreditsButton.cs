c0a9415f3a5dd61badccd93cb26b349b43d92138
junyoko1 <junyoko1@outlook.jp>
2025-12-07 09:57:29 +0900

    [ add ]タイトルに仮クレジットを追加

---
+++ Assets/Scripts/UI/CreditsButton.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CreditsButton : MonoBehaviour
+{
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private Button _creditButton;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private float _fadeDuration = 1.0f;
+    public void OnClickCredit()
+    {
+        _canvas.gameObject.SetActive(true);
+        _canvasGroup.gameObject.SetActive(true);
+        _canvasGroup.DOFade(1f, _fadeDuration);
+        _creditButton.gameObject.SetActive(false);
+
+    }
+    public void OnclickBack()
+    {
+        _canvasGroup.DOFade(0f, _fadeDuration)
+            .OnComplete(() =>
+            {
+                _canvasGroup.gameObject.SetActive(false);
+            });
+        _creditButton.gameObject.SetActive(true);
+    }
+}
