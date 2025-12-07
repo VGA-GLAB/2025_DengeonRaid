908fca4d63d9c792d084f97a95a2729468723636
junyoko1 <junyoko1@outlook.jp>
2025-12-07 10:07:16 +0900

    [ fix ] 最初からフェイドするように修正

--- Assets/Scripts/UI/CreditsButton.cs
+++ Assets/Scripts/UI/CreditsButton.cs
@@ -8,21 +8,32 @@ public class CreditsButton : MonoBehaviour
     [SerializeField] private Button _creditButton;
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private float _fadeDuration = 1.0f;
+
+    private void Start()
+    {
+        _canvasGroup.alpha = 0f;
+        _canvas.gameObject.SetActive(false);
+    }
+
     public void OnClickCredit()
     {
+       
         _canvas.gameObject.SetActive(true);
-        _canvasGroup.gameObject.SetActive(true);
-        _canvasGroup.DOFade(1f, _fadeDuration);
-        _creditButton.gameObject.SetActive(false);
 
+        _canvasGroup.alpha = 0f;
+
+        _canvasGroup.DOFade(1f, _fadeDuration);
+
+        _creditButton.gameObject.SetActive(false);
     }
+
     public void OnclickBack()
     {
         _canvasGroup.DOFade(0f, _fadeDuration)
             .OnComplete(() =>
             {
-                _canvasGroup.gameObject.SetActive(false);
+                _canvas.gameObject.SetActive(false);
+                _creditButton.gameObject.SetActive(true);
             });
-        _creditButton.gameObject.SetActive(true);
     }
 }
