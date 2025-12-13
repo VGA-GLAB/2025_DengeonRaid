594b8657b336f0e84f838e8e26c9f19c977457f2
SosukeKato <dokindokinpiano@gmail.com>
2025-12-13 18:03:02 +0900

    [update]残りの敵の数をUIに反映する処理を追加

--- Assets/Scripts/EnemyCounterUI.cs
+++ Assets/Scripts/EnemyCounterUI.cs
@@ -1,16 +1,29 @@
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyCounterUI : MonoBehaviour
 {
     [SerializeField]
+    private GameDirector _gd;
+    [SerializeField]
     private Image _enemyCountGauge;
     [SerializeField]
     private TextMeshProUGUI _enmeyCountText;
+
+    private float _restEnemyCount;
+
+    /// <summary>
+    /// 残りの敵の数をUIに反映する処理
+    /// </summary>
     public void EnemyCount()
     {
-        _enemyCountGauge.fillAmount = 0;
-        _enmeyCountText.text = "a";
+        _restEnemyCount = _gd.EnemyCountForBoss - _gd.EnemyCount;
+        if (_restEnemyCount <= 0)
+        {
+            _restEnemyCount = 0;
+        }
+        _enemyCountGauge.fillAmount = (_restEnemyCount) / _gd.EnemyCountForBoss;
+        _enmeyCountText.text = $"{_restEnemyCount}";
     }
 }
