272cf91604ba068b7508c018b3a71aeb0e1d8e30
SosukeKato <dokindokinpiano@gmail.com>
2026-01-07 18:24:17 +0900

    [update]Coinを奪っていく処理を追加

--- Assets/Scripts/CoinStealPanel.cs
+++ Assets/Scripts/CoinStealPanel.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 
-public class CoinStealPanel : MonoBehaviour
+public class CoinStealPanel : Panel
 {
-
+    [SerializeField]
+    private PreviewPlayerData playerData;
+    [SerializeField]
+    private int _stealCoinAmount;
+    public void StealCoin()
+    {
+        playerData.Money -= _stealCoinAmount;
+    }
 }
