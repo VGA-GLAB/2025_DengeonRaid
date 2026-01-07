15b2df6185d9372c959dbe32ae2dd37c5f632bea
ayato0913a <ayato0913a@gmail.com>
2026-01-07 19:42:09 +0900

    [ fux ] わかりやすいようにHeader追加

--- Assets/Scripts/System/GameDirector.cs
+++ Assets/Scripts/System/GameDirector.cs
@@ -25,8 +25,8 @@ public class GameDirector : MonoBehaviour
     private bool _bossExists;
     private int _enemyCount;
     [Header("中ボス出現要件")]
-    [SerializeField]private int _warpingMidBossCount;
-    [SerializeField]private int _enemyGenerateMidBossCount;
+    [SerializeField, Header("ワープする中ボスの出現条件（敵撃破数）")] private int _warpingMidBossCount;
+    [SerializeField, Header("子分を生成する中ボスの出現条件（敵撃破数）")] private int _enemyGenerateMidBossCount;
     [SerializeField, Header("ボスの出現条件（敵撃破数）")] private int _enemyCountForBoss;
     [SerializeField, Header("デバッグ用　現在STATE"), ReadOnly] public string CurrentState;
 
