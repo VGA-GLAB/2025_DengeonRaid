8ca921a670c42678e230854949e2e769e5d5f90c
SosukeKato <dokindokinpiano@gmail.com>
2026-01-18 17:54:47 +0900

    [update]ターン終了時にコイン奪取スキルを実行する処理を追加

--- Assets/Scripts/CoinStealPanel.cs
+++ Assets/Scripts/CoinStealPanel.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
 
-public class CoinStealPanel : Panel
+public class CoinStealPanel : EnemyPanel
 {
     [SerializeField]
     private PreviewPlayerData playerData;
     [SerializeField]
     private int _stealCoinAmount;
+    [SerializeField]
+    private int _skillInterval;
+
+    private int _turnProgress;
+
+    public void TurnEndAction()
+    {
+        _turnProgress++;
+        if (_turnProgress >= _skillInterval)
+        {
+            StealCoin();
+            _turnProgress = 0;
+        }
+    }
+
     public void StealCoin()
     {
         playerData.Money -= _stealCoinAmount;
+        if (playerData.Money <= _stealCoinAmount)
+        {
+            playerData.Money -= playerData.Money;
+        }
     }
 }
